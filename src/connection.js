462b429c0c2d418a4abffc495b0f98044e61fc36
Rmdeaguiar <rafael.aguiar91@gmail.com>
2022-11-09 18:53:33 -0300

    deploy

--- src/connection.js
+++ src/connection.js
@@ -3,12 +3,26 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        host: 'ec2-54-163-34-107.compute-1.amazonaws.com',
+        port: 5432,
+        user: 'thbkhvwnahrblc',
+        password: '0946f7fe110a1aa870172f7d08fa9a8b2bccc3b26ebafb6d1f09fe9dda958372',
+        database: 'd4g22k34q4cdpf',
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
 module.exports = knex
+
+// Host
+// ec2-54-163-34-107.compute-1.amazonaws.com
+// Database
+// d4g22k34q4cdpf
+// User
+// thbkhvwnahrblc
+// Port
+// 5432
+// Password
+// 0946f7fe110a1aa870172f7d08fa9a8b2bccc3b26ebafb6d1f09fe9dda958372
